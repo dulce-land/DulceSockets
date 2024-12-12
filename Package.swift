16ce52fb84f9d85c46bffb90981d99f9a451f9da
Daniel Norte de Moraes <danieagle@zohomail.com>
2024-12-12 14:55:12 -0300

    Backup and HouseKeeping. Thanks God and Mary and Cia!! Enjoy!! :-D

--- Package.swift
+++ Package.swift
@@ -10,15 +10,15 @@ let package = Package(
         .library(
             name: "DulceSockets",
             type: .dynamic,
-            targets: ["CDulceSockets","SDulceSockets"]
+            targets: ["SDulceSockets","CDulceSockets"]
             ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CDulceSockets", dependencies: []),
-        .target(
             name: "SDulceSockets", dependencies: ["CDulceSockets"]),
+        .target(
+            name: "CDulceSockets", dependencies: []),
     ]
 )
